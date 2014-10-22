28e8b3d615e817665de291c9d09379fc266f0bd7
Rohan Aurora <rohanaurora@gmail.com>
2014-10-21 23:54:01 -0700

    Update Credit Card classes.

--- CreditCardInfo.h
+++ CreditCardInfo.h
@@ -1,5 +1,6 @@
 //
 //  CreditCardInfo.h
+//  ZoneTest
 //
 //  Created by Rohan Aurora on 10/20/14.
 //  Copyright (c) 2014 Rohan Aurora. All rights reserved.
@@ -25,7 +26,7 @@
 @property (nonatomic ,copy, readwrite) NSString *expireYear;
 
 /// Security code (aka CSC, CVV, CVV2, etc.) Required.
-@property (nonatomic ,copy, readwrite) NSString *cvv2;
+@property (nonatomic ,copy, readwrite) NSString *cvv;
 
 /// First name of the credit card holder.
 @property (nonatomic ,copy, readwrite) NSString *firstName;
@@ -39,13 +40,13 @@
 /// Billing address line 2 of the credit card holder.
 @property (nonatomic ,copy, readwrite) NSString *billingAddressLine2;
 
-/// City name. 50 characters max. Required.
+/// City name. Required.
 @property (nonatomic ,copy, readwrite) NSString *billingAddressCity;
 
 /// Postal code. Format is country dependent. Required.
 @property (nonatomic ,copy, readwrite) NSString *billingAddressPostalCode;
 
-/// 2-letter code for US states, and the equivalent for other countries. 100 characters max. Required.
+/// 2-letter code for US states, and the equivalent for other countries. Required.
 @property (nonatomic ,copy, readwrite) NSString *billingAddressState;
 
 /// 2-letter country code. 2 characters max. Required.
@@ -59,7 +60,7 @@
  *  @param number                    Credit Card number.
  *  @param expireMonth               Credit Card expiration month.
  *  @param expireYear                Credit Card expiration year.
- *  @param cvv2                      Credit Card security code.
+ *  @param cvv                       Credit Card security code.
  *  @param firstName                 Credit Card holder's first name.
  *  @param lastName                  Credit Card holder's last name.
  *  @param billingAddressLine1       Credit Card billing address line 1.
@@ -72,31 +73,22 @@
  *  @return CreditCardInfo object
  */
 
--(instancetype)initWithType:(NSString *)type
 
-                     number:(NSString *)number
 
-                expireMonth:(NSString *)expireMonth
++ (CreditCardInfo *)initWithType:(NSString *)type
+                          number:(NSString *)number
+                     expireMonth:(NSString *)expireMonth
+                      expireYear:(NSString *)expireYear
+                             cvv:(NSString *)cvv
+                       firstName:(NSString *)firstName
+                        lastName:(NSString *)lastName
+             billingAddressLine1:(NSString *)billingAddressLine1
+             billingAddressLine2:(NSString *)billingAddressLine2
+              billingAddressCity:(NSString *)billingAddressCity
+        billingAddressPostalCode:(NSString *)billingAddressPostalCode
+             billingAddressState:(NSString *)billingAddressState
+       billingAddressCountryCode:(NSString *)billingAddressCountryCode;
 
-                 expireYear:(NSString *)expireYear
-
-                       cvv2:(NSString *)cvv2
-
-                  firstName:(NSString *)firstName
-
-                   lastName:(NSString *)lastName
-
-        billingAddressLine1:(NSString *)billingAddressLine1
-
-        billingAddressLine2:(NSString *)billingAddressLine2
-
-         billingAddressCity:(NSString *)billingAddressCity
-
-   billingAddressPostalCode:(NSString *)billingAddressPostalCode
-
-        billingAddressState:(NSString *)billingAddressState
-
-  billingAddressCountryCode:(NSString *)billingAddressCountryCode;
 
 @end
 
--- CreditCardInfo.m
+++ CreditCardInfo.m
@@ -10,24 +10,24 @@
 @implementation CreditCardInfo
 
 + (CreditCardInfo *)initWithType:(NSString *)type
-                          number:(NSString*)number
-                     expireMonth:(NSString*)expireMonth
-                      expireYear:(NSString*)expireYear
-                            cvv2:(NSString*)cvv2
-                       firstName:(NSString*)firstName
-                        lastName:(NSString*)lastName
-             billingAddressLine1:(NSString*)billingAddressLine1
-             billingAddressLine2:(NSString*)billingAddressLine2
-              billingAddressCity:(NSString*)billingAddressCity
-        billingAddressPostalCode:(NSString*)billingAddressPostalCode
-             billingAddressState:(NSString*)billingAddressState
-       billingAddressCountryCode:(NSString*)billingAddressCountryCode{
+                          number:(NSString *)number
+                     expireMonth:(NSString *)expireMonth
+                      expireYear:(NSString *)expireYear
+                             cvv:(NSString *)cvv
+                       firstName:(NSString *)firstName
+                        lastName:(NSString *)lastName
+             billingAddressLine1:(NSString *)billingAddressLine1
+             billingAddressLine2:(NSString *)billingAddressLine2
+              billingAddressCity:(NSString *)billingAddressCity
+        billingAddressPostalCode:(NSString *)billingAddressPostalCode
+             billingAddressState:(NSString *)billingAddressState
+       billingAddressCountryCode:(NSString *)billingAddressCountryCode{
     
     CreditCardInfo *creditCard = [CreditCardInfo new];
     creditCard.type = type;
     creditCard.number = number;
     creditCard.expireMonth = expireMonth;
-    creditCard.cvv2 = cvv2;
+    creditCard.cvv = cvv;
     creditCard.firstName = firstName;
     creditCard.lastName = lastName;
     creditCard.billingAddressLine1 = billingAddressLine1;
@@ -40,17 +40,22 @@
 }
 
 
-//- (CreditCardInfo *)copyWithZone:(NSZone *)zone {
-//    CreditCardInfo *clone = [[CreditCardInfo allocWithZone:zone] init];
-//    clone.recipientName = [self.recipientName copyWithZone:zone];
-//    clone.line1 = [self.line1 copyWithZone:zone];
-//    clone.line2 = [self.line2 copyWithZone:zone];
-//    clone.city = [self.city copyWithZone:zone];
-//    clone.state = [self.state copyWithZone:zone];
-//    clone.zipCode = [self.zipCode copyWithZone:zone];
-//    clone.countryCode = [self.countryCode copyWithZone:zone];
-//    return clone;
-//}
+- (CreditCardInfo *)copyWithZone:(NSZone *)zone {
+    CreditCardInfo *clone = [[CreditCardInfo allocWithZone:zone] init];
+    clone.type = [self.type copyWithZone:zone];
+    clone.number = [self.number copyWithZone:zone];
+    clone.expireMonth = [self.expireMonth copyWithZone:zone];
+    clone.cvv = [self.cvv copyWithZone:zone];
+    clone.firstName = [self.firstName copyWithZone:zone];
+    clone.lastName = [self.lastName copyWithZone:zone];
+    clone.billingAddressLine1 = [self.billingAddressLine1 copyWithZone:zone];
+    clone.billingAddressLine2 = [self.billingAddressLine2 copyWithZone:zone];
+    clone.billingAddressCity = [self.billingAddressCity copyWithZone:zone];
+    clone.billingAddressPostalCode = [self.billingAddressPostalCode copyWithZone:zone];
+    clone.billingAddressState = [self.billingAddressState copyWithZone:zone];
+    clone.billingAddressCountryCode = [self.billingAddressCountryCode copyWithZone:zone];
+    return clone;
+}
 
 
 @end
